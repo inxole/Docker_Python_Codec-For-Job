cf7f27d3d4f04ec01b71a55933e25427be3cc057
ShortArrow <bamboogeneral@shortarrow.jp>
2024-08-28 20:59:22 +0900

    rename compose file

--- destroy.sh
+++ destroy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker compose -f ./frontend/docker-compose.yaml down -v
-docker compose -f ./backend/docker-compose.yaml down -v
+docker compose -f ./frontend/compose.yaml down -v
+docker compose -f ./backend/compose.yaml down -v
 
--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 rm -rf ./frontend/node_modules
 rm -rf ./frontend/.pnpm-store
 
-docker compose -f ./frontend/docker-compose.yaml up -d --build
-docker compose -f ./backend/docker-compose.yaml up -d --build
+docker compose -f ./frontend/compose.yaml up -d --build
+docker compose -f ./backend/compose.yaml up -d --build
 
