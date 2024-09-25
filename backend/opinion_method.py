f6128fdeca007ca1914e0112521bdfffe40b3e14
inxole <inxole@gmail.com>
2024-09-25 17:16:52 +0900

    add sample code that create database

--- backend/opinion_method.py
+++ backend/opinion_method.py
@@ -1,8 +1,32 @@
 """pdf to dxf converter script"""
 
 import uuid
+from typing import Optional
 from pydantic import BaseModel
 from fastapi import APIRouter
+from sqlmodel import Field, Session, SQLModel, create_engine
+
+
+class Hero(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: Optional[int] = None
+
+
+hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+
+engine = create_engine("mysql://test:test@db/test")
+
+SQLModel.metadata.create_all(engine)
+
+with Session(engine) as session:
+    session.add(hero_1)
+    session.add(hero_2)
+    session.add(hero_3)
+    session.commit()
 
 
 router = APIRouter()
